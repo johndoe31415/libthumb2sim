835d1939b40aae9162214f28ab99b4e773bd4dd2
Johannes Bauer <joe@johannes-bauer.com>
2019-02-17 15:10:25 +0100

    Add example to run Cortex-M code on QEmu
    Might be useful to have a small stub to test code on in QEmu to verify
    integrity without having to have access to the actual hardware.

---
+++ qemu/qemu_test.s
@@ -0,0 +1,40 @@
+/**
+ *	flightpanel - A Cortex-M4 based USB flight panel for flight simulators.
+ *	Copyright (C) 2017-2017 Johannes Bauer
+ *
+ *	This file is part of flightpanel.
+ *
+ *	flightpanel is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; this program is ONLY licensed under
+ *	version 3 of the License, later versions are explicitly excluded.
+ *
+ *	flightpanel is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with flightpanel; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *	Johannes Bauer <JohannesBauer@gmx.de>
+**/
+
+.syntax unified
+.cpu cortex-m4
+.fpu softvfp
+.thumb
+
+.section .text
+.type main, %function
+main:
+	b main
+.size main, .-main
+.global main
+
+.type default_fault_handler, %function
+default_fault_handler:
+	b default_fault_handler
+.size default_fault_handler, .-default_fault_handler
+.global default_fault_handler
