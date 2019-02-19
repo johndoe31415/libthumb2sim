ef709e05fb84af79d0699b3d80e93041ada9aa43
Johannes Bauer <joe@johannes-bauer.com>
2019-02-19 23:29:44 +0100

    More tracing options
    Really easy to add more features to this, now we've implemented quite a
    cool set of tracing features.

--- qemu/run_testcase.py
+++ qemu/run_testcase.py
@@ -31,17 +31,36 @@ import socket
 import hashlib
 import struct
 import zlib
+import json
+import datetime
+import base64
 from FriendlyArgumentParser import FriendlyArgumentParser, baseint
 from DebuggingProtocol import ARMDebuggingProtocol
 
 parser = FriendlyArgumentParser()
 parser.add_argument("--bin-data", choices = [ "compressed", "uncompressed", "omit" ], default = "compressed", help = "For every trace step, include raw binary data. Can be in compressed form, uncompressed form or entirely ommited. Defaults to %(default)s.")
+parser.add_argument("--bin-format", choices = [ "b64", "hex" ], default = "b64", help = "For binary data, chooses the representation inside JSON. Can be one of %(choices)s, defaults to %(default)s.")
 parser.add_argument("--omit-raw-registers", action = "store_true", help = "By default, for every trace step, all register values as well as the PSR is included, not just the hashed values. This omits the raw data as well.")
 parser.add_argument("--rom-base", metavar = "address", type = baseint, default = 0, help = "ROM base address, defaults to 0x%(default)x bytes.")
 parser.add_argument("--ram-base", metavar = "address", type = baseint, default = 0x20000000, help = "RAM base address, defaults to 0x%(default)x bytes.")
 parser.add_argument("--ram-size", metavar = "length", type = baseint, default = 0x10000, help = "RAM size, defaults to 0x%(default)x bytes.")
-parser.add_argument("bin_image", metavar = "filename", type = str, help = "Binary image which to load into QEMU for tracing")
+parser.add_argument("-d", "--decimation", metavar = "step", type = int, default = 1, help = "Record only every n-th step, i.e., decimate trace data by a factor of n. By default, n is %(default)d (i.e., no decimation occurs).")
+parser.add_argument("--pretty-json", action = "store_true", help = "Output a nicely formatted, human-readable JSON document.")
+parser.add_argument("img_filename", metavar = "image_filename", type = str, help = "Binary image which to load into QEMU for tracing")
+parser.add_argument("trc_filename", metavar = "trace_filename", type = str, help = "JSON trc_filename to write")
 args = parser.parse_args(sys.argv[1:])
+
+class CustomJSONEncoderB64(json.JSONEncoder):
+	def default(self, obj):
+		if isinstance(obj, bytes):
+			return base64.b64encode(obj).decode("ascii")
+		return json.JSONEncoder.default(self, obj)
+
+class CustomJSONEncoderHex(json.JSONEncoder):
+	def default(self, obj):
+		if isinstance(obj, bytes):
+			return obj.hex()
+		return json.JSONEncoder.default(self, obj)
 
 class TraceType(object):
 	def __init__(self, args):
@@ -50,13 +69,11 @@ class TraceType(object):
 	def _add_data(self, result_dict, data):
 		if self._args.bin_data == "compressed":
 			result_dict.update({
-				"data":			zlib.compress(data),
-				"data_type":	"compressed",
+				"raw":			zlib.compress(data),
 			})
-		elif self._args.bin_data == "compressed":
+		elif self._args.bin_data == "uncompressed":
 			result_dict.update({
-				"data":			data,
-				"data_type":	"uncompressed",
+				"raw":			data,
 			})
 
 class ConstantValue(TraceType):
@@ -81,7 +98,7 @@ class ConstantValue(TraceType):
 
 	def get(self, target):
 		return {
-			"result":	self._value,
+			"value":	self._value,
 		}
 
 class RegisterHash(TraceType):
@@ -97,7 +114,7 @@ class RegisterHash(TraceType):
 		packed.append(regs["psr"] & 0xf8000000)
 		data = self._RegStruct.pack(*packed)
 		result = {
-			"result":	hashlib.md5(data).digest()
+			"value":	hashlib.md5(data).digest()
 		}
 		self._add_data(result, data)
 		if not self._args.omit_raw_registers:
@@ -105,21 +122,21 @@ class RegisterHash(TraceType):
 		return result
 
 class MemoryHash(TraceType):
-	def __init__(self, args, name, address, length, is_constant):
+	def __init__(self, args, region_name, address, length, is_constant):
 		TraceType.__init__(self, args)
-		self._name = name
+		self._region_name = region_name
 		self._address = address
 		self._length = length
 		self._is_constant = is_constant
 
 	@property
 	def name(self):
-		return "memory_hash"
+		return "memory_hash/%s" % (self._region_name)
 
 	def get(self, target):
 		data = target.read_memory(self._address, self._length)
 		result = {
-			"result":	hashlib.md5(data).digest()
+			"value":	hashlib.md5(data).digest()
 		}
 		self._add_data(result, data)
 		return result
@@ -136,16 +153,38 @@ class TraceFile(object):
 	def set_prev_state(self, previous_state):
 		self._previous_state.value = previous_state
 
-	def determine_state(self, target):
+	def record_state(self, target):
 		all_state = bytearray()
+
+		components = [ ]
 		for tracetype in self._tracetypes:
-			tracehash = tracetype.get(target)
-			print(tracehash)
-			#all_state += tracehash
-		print(all_state)
+			component = tracetype.get(target)
+#			component["name"] = tracetype.name
+			components.append(component)
+			all_state += component["value"]
 		state_hash = hashlib.md5(all_state).digest()
-		print(state_hash)
-		return state_hash
+		self._previous_state.value = state_hash
+		tracepoint = {
+			"executed_insns":	len(self._trace),
+			"components":		components,
+			"state_hash":		state_hash,
+		}
+		self._trace.append(tracepoint)
+
+	def write(self, filename):
+		data = {
+			"meta": {
+				"trace_date_utc":		datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+			},
+			"structure": {
+				# TODO: Only have repeating info (name, compression, etc) appear once
+			},
+			"trace": self._trace,
+		}
+		print(self._trace)
+		with open(filename, "w") as f:
+			json.dump(data, f, cls = CustomJSONEncoderB64 if (self._args.bin_format == "b64") else CustomJSONEncoderHex, sort_keys = self._args.pretty_json, indent = None if (not self._args.pretty_json) else 4)
+			f.write("\n")
 
 def get_filesize(filename):
 	with open(filename, "rb") as f:
@@ -154,7 +193,7 @@ def get_filesize(filename):
 
 trace = TraceFile(args, [
 	RegisterHash(args),
-	MemoryHash(args, "rom", args.rom_base, get_filesize(args.bin_image), is_constant = True),
+	MemoryHash(args, "rom", args.rom_base, get_filesize(args.img_filename), is_constant = True),
 	MemoryHash(args, "ram", args.ram_base, args.ram_size, is_constant = False),
 ])
 
@@ -168,16 +207,18 @@ with tempfile.NamedTemporaryFile(prefix = "qemu_gdb_") as f:
 
 	# Then fire up QEMU and have it connect to socket
 	try:
-		qemu_process = subprocess.Popen([ "qemu-system-arm", "-S", "-machine", "lm3s6965evb", "-display", "none", "-monitor", "none", "-gdb", "unix:%s" % (f.name), "-kernel", args.bin_image ])
+		qemu_process = subprocess.Popen([ "qemu-system-arm", "-S", "-machine", "lm3s6965evb", "-display", "none", "-monitor", "none", "-gdb", "unix:%s" % (f.name), "-kernel", args.img_filename ])
 
 		# Accept debugging connection from QEMU
 		(conn, peer_address) = sock.accept()
 		conn.settimeout(0.1)
 
 		with ARMDebuggingProtocol(conn) as dbg:
-			trace.determine_state(dbg)
-#			dbg.dump_regs()
-#			dbg.singlestep()
+			trace.record_state(dbg)
+			dbg.singlestep()
+			trace.record_state(dbg)
 
 	finally:
 		qemu_process.kill()
+
+trace.write(args.trc_filename)
